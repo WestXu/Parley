7ad30ed62236ab911b65570d6e223ce1000c5a91
WestXu <west@westxu.com>
2026-06-09 19:53:08 +0800

    Add Turkish support

--- src/soniox.ts
+++ src/soniox.ts
@@ -1,6 +1,6 @@
-export type Lang = "en" | "zh" | "vi" | "ja" | "th" | "nl" | "pt" | "fr" | "es" | "ru" | "ko"
+export type Lang = "en" | "zh" | "vi" | "ja" | "th" | "nl" | "pt" | "fr" | "es" | "ru" | "ko" | "tr"
 
-export const LANGS: Lang[] = ["en", "zh", "vi", "ja", "th", "nl", "pt", "fr", "es", "ru", "ko"]
+export const LANGS: Lang[] = ["en", "zh", "vi", "ja", "th", "nl", "pt", "fr", "es", "ru", "ko", "tr"]
 
 export type Token = {
   text: string
